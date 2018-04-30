efd3a713e685f2cac2e82b6c6ec122a7018335a3
MaxMSchweitzer <mschwei2@pdx.edu>
2018-04-29 20:54:55 -0700

    User space better, needs some more changes

--- HW3/User/main.c
+++ HW3/User/main.c
@@ -1,8 +1,8 @@
 /* Max Schweitzer
    ECE 373
-   Homework 2 - Char Driver
+   Homework 3 - PCI LED Driver
    This code implements a user space program to
-   test a basic char driver.
+   test a basic PCI driver that can blink an LED.
    4/22/18
 */
 
@@ -19,14 +19,7 @@
 int main()
 {
   int fd;
-
-  //char receive;
-  int send = 0;
   uint32_t toSend = 0;
-
-  //char toSend[LENGTH] = {0};
-
-  //strcpy(toSend, "20");
 
   fd = open("/dev/hw2_kernel", O_RDWR);
   if (fd < 0)
@@ -61,7 +54,6 @@ int main()
   }
 
   // Overwrite before we read again.
-  //strcpy(toSend, "0");
   toSend = 0;
 
   ret = read(fd, &toSend, sizeof(uint32_t));
